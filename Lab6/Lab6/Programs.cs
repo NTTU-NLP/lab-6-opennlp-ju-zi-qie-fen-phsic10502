535825b8d4013d8980547e49e1c507a0b7e056a6
phsic10502 <phsic10502@users.noreply.github.com>
2017-04-09 23:28:41 +0800

    finished

---
+++ Lab6/Lab6/Programs.cs
@@ -0,0 +1,36 @@
+﻿using opennlp.tools.sentdetect;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lab6
+{
+    class Program
+    {
+        static SentenceDetector detector;
+
+        static void lab5(String src)
+        {
+            StreamReader input = new StreamReader(src);
+            StreamWriter output = new StreamWriter(Regex.Replace(src, "(.*).txt", "$1_Sent.txt"));
+            while (input.Peek() != -1)
+            {
+                string dataLine = input.ReadLine();
+                string[] sents = detector.sentDetect(dataLine);
+                for (int i = 0; i < sents.Length; i++)
+                    output.WriteLine(sents.GetValue(i));
+                output.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            java.io.InputStream model_src = new java.io.FileInputStream(@"..\..\en-sent.bin");
+            SentenceModel smodel = new SentenceModel(model_src);
+            detector = new SentenceDetectorME(smodel);
+
+            foreach (String src in Directory.GetFiles(@"..\..\..\..\Dataset"))
+                lab5(src);
+        }
+    }
+}
